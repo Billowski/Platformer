a570b3a72459e77e78b132a807413abc13607863
Billu <Billowski98@gmail.com>
2020-12-07 19:02:19 +0100

    Dodanie projektu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+
+
+    public Animator anim;
+    public Transform pivot;
+    public float rotateSpeed;
+
+    public GameObject playerModel;
+    public float knockBackForce;
+    public float knockBackTime;
+    private float knockBackCounter;
+
+
+
+    // Start is called before the first frame update
+    private void Start()
+    { 
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (knockBackCounter <= 0)
+        {
+            float yStore = moveDirection.y;
+            moveDirection = (transform.forward * Input.GetAxisRaw("Vertical") * moveSpeed) + (transform.right * Input.GetAxisRaw("Horizontal") * moveSpeed);
+            moveDirection = moveDirection.normalized * moveSpeed;
+            moveDirection.y = yStore;
+            if (controller.isGrounded)
+            {
+                moveDirection.y = 0f;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveDirection.y = jumpForce;
+                }
+            }
+        }
+        else
+        { 
+            knockBackCounter -= Time.deltaTime;
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Move the player in different directions based on camera look direction
+        if(Input.GetAxisRaw("Horizontal") !=0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+        
+        anim.SetBool("IsGrounded", controller.isGrounded);
+        anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxisRaw("Vertical")) + Mathf.Abs(Input.GetAxisRaw("Horizontal"))));
+    }
+
+    public void Knockback(Vector3 direction)
+    {
+        knockBackCounter = knockBackTime;
+
+        moveDirection = direction * knockBackForce;
+        moveDirection.y = knockBackForce;
+    }
+}
